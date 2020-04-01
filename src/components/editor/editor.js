ef80562050d0d9ec982d259281b058f8a3241cc8
Debarun Mukherjee <debarun.mukherjee1997@gmail.com>
2020-04-01 18:58:23 +0530

    Changed settings for production deploy

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -138,7 +138,7 @@ export default withRouter(function(props) {
 		if (username && valueGetter.current() != default_code_to_show) {
 			const cur_code = valueGetter.current();
 
-			fetch("http://api.contest-arena/code", {
+			fetch("http://13.232.146.140/code", {
 				method: "POST",
 				headers: {
 					"content-Type": "application/json"
@@ -193,7 +193,7 @@ export default withRouter(function(props) {
 				setCurCode(cur_code);
 			} else {
 				fetch(
-					`http://api.contest-arena/code/${props.contest_code}/${props.problem_code}/${username}`,
+					`http://13.232.146.140/code/${props.contest_code}/${props.problem_code}/${username}`,
 					{
 						method: "GET",
 						headers: {
